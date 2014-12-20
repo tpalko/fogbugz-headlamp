b3e9b89b4cb25ae3686cc310bc0a73900ea1b808
Tim Palko <tim@palkosoftware.com>
2014-12-20 00:23:48 -0500

    latest changes from biereetvin

--- webapp/milestones.py
+++ webapp/milestones.py
@@ -1,12 +1,16 @@
 #?token=quadespiooa20s5ghq507mkegula65
 
-
+import sys
 from flask import Flask
 from flask import render_template
 from flask import request
 from fogbugz import FogBugz
 import re
 from datetime import datetime
+import traceback
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 app.debug = True
@@ -51,69 +55,105 @@ def parse_cdata(string):
 
 def get_person_name(ixPerson):
 
+	if ixPerson is None or int(ixPerson) == 0:
+		return None
+
 	if ixPerson not in person_names:
 		response = fb.viewPerson(ixPerson=ixPerson)		
 		person_names[ixPerson] = parse_cdata(response.person.sfullname.string)
 	
 	return person_names[ixPerson]
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def milestones():
 
-	filter = request.args.get('filter', None)
+	try:
 
-	fixforlist = fb.listFixFors()
+		milestone_filter = None
+		person_filter = None
+		lastmilestone_filter = None
 
-	fixfors = []
-	now = datetime.now()
+		if request.method == "POST":
+			milestone_filter = request.form['milestone_filter'] if request.form['milestone_filter'] != "" else ""
+			person_filter = request.form['person_filter'] if request.form['person_filter'] != "" else None
+			lastmilestone_filter = request.form['lastmilestone_filter'] if request.form['lastmilestone_filter'] != "" else None
 
-	total_cost = 0
+		fixforlist = fb.listFixFors()
 
-	for f in fixforlist.findAll('fixfor'):
+		#logging.debug(fixforlist.prettify())
 
-		if f.dt.string is None:
-			continue
-		
-		sfixfor = parse_cdata(f.sfixfor.contents[0]) if len(f.sfixfor.contents) > 0 else "-"
+		fixfors = []
+		milestones_filter = []
+		now = datetime.now()
+		cutoff = now
 
-		if filter is not None and filter not in sfixfor:
-			continue
+		if lastmilestone_filter is not None and lastmilestone_filter != "":
+			lastmilestone = fixforlist.findAll(text=re.compile("^%s$" %(lastmilestone_filter)))
+			if len(lastmilestone) > 0:
+				cutoff = datetime.strptime(lastmilestone[0].parent.parent.dt.string, "%Y-%m-%dT%H:%M:%SZ")
+				logging.debug(cutoff)
 
-		startdate = None
+		total_cost = 0
 
-		# -- 2013-10-03T04:00:00Z	
-		if f.dtstart.string is not None:
-			startdate = datetime.strptime(f.dtstart.string, "%Y-%m-%dT%H:%M:%SZ")
+		for f in fixforlist.findAll('fixfor'):
 
-		enddate = datetime.strptime(f.dt.string, "%Y-%m-%dT%H:%M:%SZ")
+			if f.dt.string is None:
+				continue
+			
+			sfixfor = parse_cdata(f.sfixfor.contents[0]) if len(f.sfixfor.contents) > 0 else "-"
 
-		if enddate < now:
+			if milestone_filter is not None and milestone_filter not in sfixfor:
+				continue
 
-			fixfors.append(fixfor(
-					f.ixfixfor.string, 				
-					sfixfor,
-					f.ixproject.string, 
-					parse_cdata(f.sproject.contents[0]) if len(f.sproject.contents) > 0 else "-",
-					enddate, 
-					startdate))
+			startdate = None
 
-	caselist = fb.search(q="", cols="ixFixFor,ixProject,sStatus,ixPersonResolvedBy,hrsElapsed,hrsElapsedExtra")
+			# -- 2013-10-03T04:00:00Z	
+			if f.dtstart.string is not None:
+				startdate = datetime.strptime(f.dtstart.string, "%Y-%m-%dT%H:%M:%SZ")
 
-	description_tag = caselist.find("description")	
+			enddate = datetime.strptime(f.dt.string, "%Y-%m-%dT%H:%M:%SZ")
 
-	description = parse_cdata(description_tag.string)
+			milestones_filter.append(fixfor(
+				f.ixfixfor.string, 				
+				sfixfor,
+				f.ixproject.string, 
+				parse_cdata(f.sproject.contents[0]) if len(f.sproject.contents) > 0 else "-",
+				enddate, 
+				startdate))
 
-	for c in caselist.findAll('case'):
+			# - we're invoicing whole milestones, so we don't want to factor in milestones that haven't ended yet
+			if enddate <= cutoff:
 
-		if c.ixfixfor is None:
-			continue
+				fixfors.append(fixfor(
+						f.ixfixfor.string, 				
+						sfixfor,
+						f.ixproject.string, 
+						parse_cdata(f.sproject.contents[0]) if len(f.sproject.contents) > 0 else "-",
+						enddate, 
+						startdate))
 
-		if not c.sstatus.contents[0].find("Resolved") and not c.sstatus.contents[0].find("Closed"):
-			continue
+		caselist = fb.search(q="", cols="ixFixFor,ixProject,sStatus,ixPersonResolvedBy,hrsElapsed,hrsElapsedExtra")
 
-		f = get_fixfor_by_id(c.ixfixfor.string, fixfors)
-		
-		if f is not None:
+		description_tag = caselist.find("description")	
+
+		description = parse_cdata(description_tag.string)
+
+		# - find all relevant cases
+		for c in caselist.findAll('case'):
+
+			if c.ixfixfor is None:
+				continue
+
+			if not c.sstatus.contents[0].find("Resolved") and not c.sstatus.contents[0].find("Closed"):
+				continue
+
+			f = get_fixfor_by_id(c.ixfixfor.string, fixfors)
+			
+			if f is None:
+				continue
+
+			if person_filter is not None and int(c.ixpersonresolvedby.string) != int(person_filter):
+				continue
 
 			f.cases.append(case(
 				c.ixpersonresolvedby.string,
@@ -121,23 +161,34 @@ def milestones():
 				c.hrselapsedextra.string)
 			)
 
-	for f in fixfors:
+		# - for each case in each milestone, figure hours per person per case, and total cost for the milestone
+		for f in fixfors:
+			
+			for c in f.cases:
+
+				person_name = get_person_name(c.ixPersonResolvedBy)				
+
+				if person_name is None:
+					continue
+					
+				if person_name not in f.person_hours:				
+					f.person_hours[person_name] = 0
+
+				f.person_hours[person_name] += c.hrsElapsed
+				f.cost += c.hrsElapsed*rates[person_name]
+
+			total_cost += f.cost
+
+		fixfors = sorted(fixfors, key=lambda fixfor: fixfor.dt, reverse=True)
+		milestones_filter = sorted(milestones_filter, key=lambda fixfor: fixfor.dt, reverse=True)
+
+		return render_template('milestones.html', person_names=person_names, milestones_filter=milestones_filter, milestone_filter=milestone_filter, person_filter=person_filter, lastmilestone_filter=lastmilestone_filter, total_cost=total_cost, description=description, fixfors=fixfors)
+
+	except:
 		
-		for c in f.cases:
-
-			person_name = get_person_name(c.ixPersonResolvedBy)				
-
-			if person_name not in f.person_hours:				
-				f.person_hours[person_name] = 0
-
-			f.person_hours[person_name] += c.hrsElapsed
-			f.cost += c.hrsElapsed*rates[person_name]
-
-		total_cost += f.cost
-
-	fixfors = sorted(fixfors, key=lambda fixfor: fixfor.dt, reverse=True)
-
-	return render_template('milestones.html', filter=filter, total_cost=total_cost, description=description, fixfors=fixfors)
+		print sys.exc_info()
+		traceback.print_tb(sys.exc_info()[2])
+		return render_template('error.html', error=sys.exc_info()[2])
 
 if __name__ == "__main__":
 	app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,40 @@
-import sys
-sys.path.insert(0, '/home/tpalko/biereetvin_development/code/milestone_invoicing')
-
 from webapp.milestones import app as application
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
