939d425949b6800e511613b796704a1f3b3b7baf
Tim Palko <tpalko@cert.org>
2016-11-09 03:08:11 -0500

    added Procfile. added code to parse .env and usage of config_[environment].py based on envvar. added wsgi.py

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+import os
+
+with open('.env', 'rb') as envfile:
+	lines = envfile.readlines()
+	for l in lines:
+		envvar, value = l.split('=')
+		os.environ[envvar] = value
+		print "loaded %s=%s" %(envvar, value)
+
 from webapp import app
 from flask.ext.script import Manager, Server
 from flask.ext.migrate import MigrateCommand
